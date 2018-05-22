ef7859b3879799fea24cb5c3925aff042de48cd9
Davide <you@example.com>
2018-05-22 12:13:18 +0200

    finita parte dei tags

--- posts-tag.php
+++ posts-tag.php
@@ -10,35 +10,34 @@
          include 'data.php';
          // prendo i dati del form
          $tag = $_GET;
+         $tagToSearch = $tag['tagChoice'];
          // faccio un ciclo nell'array di posts
          foreach ($posts as $post) {
-            foreach ($post['tag'] as $singleTag) {
-               if($tag['tagChoice'] == $singleTag){
+            if(in_array($tagToSearch, $post['tag'])){
       ?>
-                  <section id="title_cnt">
-                     <h1 class="postTitle"> <?php echo $post['title']; ?> </h1>
-                     <div class="data">
-                        <?php echo $post['published_at']; ?>
-                     </div>
-                  </section>
-                  <div class="text_cnt">
-                     <img class="text-img" src= <?php echo $post['image'] ?> >
-                     <p> <?php echo $post['content']; ?> </p>
+               <section id="title_cnt">
+                  <h1 class="postTitle"> <?php echo $post['title']; ?> </h1>
+                  <div class="data">
+                     <?php echo $post['published_at']; ?>
                   </div>
-                  <div class="tags">
-                     Tags:
-                     <?php
-                        foreach ($post['tag'] as $key => $tag) {
-                           if($key == (count($post['tag']) - 1)){
-                              echo $tag;
-                           }else{
-                              echo $tag . ', ';
-                           }
+               </section>
+               <div class="text_cnt">
+                  <img class="text-img" src= <?php echo $post['image'] ?> >
+                  <p> <?php echo $post['content']; ?> </p>
+               </div>
+               <div class="tags">
+                  Tags:
+                  <?php
+                     foreach ($post['tag'] as $key => $tag) {
+                        if($key == (count($post['tag']) - 1)){
+                           echo $tag;
+                        }else{
+                           echo $tag . ', ';
                         }
-                     ?>
-                  </div>
-                <?php
-               }
+                     }
+                  ?>
+               </div>
+             <?php
             }
          }
          ?>
